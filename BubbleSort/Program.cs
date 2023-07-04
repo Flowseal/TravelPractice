cbb22920fe82f32afe15feffe6bd4f7ac37cd7f3
Flowseal <trololo424@yandex.ru>
2023-07-04 13:26:25 +0300

    3 july

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿void BubbleSort<T>( List<T> list ) where T : IComparable<T>
+{
+    for ( int i = 0; i < list.Count - 1; i++ )
+    {
+        for ( int j = 0; j < list.Count - i - 1; j++ )
+        {
+            if ( list[ j ].CompareTo( list[ j + 1 ] ) > 0 )
+            {
+                T tempElement = list[ j ];
+                list[ j ] = list[ j + 1 ];
+                list[ j + 1 ] = tempElement;
+            }
+        }
+    }
+}
+
+List<int> listOfIntegers = new()
+{
+    15,
+    3,
+    1,
+    5,
+    22,
+    7
+};
+
+BubbleSort( listOfIntegers );
+Console.WriteLine( "Sorted list of integers:" );
+listOfIntegers.ForEach( num => Console.WriteLine( num ) );
+
+List<char> listOfChars = new()
+{
+    'a',
+    'c',
+    'b',
+    'd',
+    'j',
+    'e'
+};
+
+BubbleSort( listOfChars );
+Console.WriteLine( "\nSorted list of chars:" );
+listOfChars.ForEach( ch => Console.WriteLine( ch ) );
---
+++ Information/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+Encoding.RegisterProvider( CodePagesEncodingProvider.Instance );
+Console.OutputEncoding = Encoding.GetEncoding( 1251 );
+Console.InputEncoding = Encoding.GetEncoding( 1251 );
+
+Console.WriteLine( "Введите следующую информацию о себе:" );
+
+Console.Write( "Имя: " );
+string firstName = Console.ReadLine();
+
+Console.Write( "Фамилия: " );
+string lastName = Console.ReadLine();
+
+Console.Write( "Отчество: " );
+string middleName = Console.ReadLine();
+
+string rawAge = String.Empty;
+int age = -1;
+bool firstReadingAge = true;
+
+while ( !int.TryParse(rawAge, out age) || age < 0)
+{
+    if ( !firstReadingAge )
+        Console.WriteLine( "Ошибка при чтении возраста! Введите положительное число." );
+
+    Console.Write( "Возраст: " );
+    rawAge = Console.ReadLine();
+
+    firstReadingAge = false;
+}
+
+Console.Write( "Email: " );
+string email = Console.ReadLine();
+
+Console.Write( "Github: " );
+string github = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine( $"Информация о пользователе {lastName} {firstName[ 0 ]}.{middleName[ 0 ]}." );
+
+Console.WriteLine( "Личная информация:" );
+Console.WriteLine( $"\tИмя: {firstName}" );
+Console.WriteLine( $"\tФамилия: {lastName}" );
+Console.WriteLine( $"\tОтчество: {middleName}" );
+Console.WriteLine( $"\tВозраст: {age}" );
+
+Console.WriteLine( "Контактная информация:" );
+Console.WriteLine( $"\tEmail: {email}" );
+Console.WriteLine( $"\tGithub: {github}" );
