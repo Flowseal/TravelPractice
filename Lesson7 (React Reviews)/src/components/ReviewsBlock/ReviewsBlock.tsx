4729cbbdb602d05757ea8ba39368e51f86866e9b
Flowseal <trololo424@yandex.ru>
2023-07-28 23:13:35 +0300

    closes #6; (Lesson 7) Code simplification

--- Lesson7 (React Reviews)/src/components/ReviewsBlock/ReviewsBlock.tsx
+++ Lesson7 (React Reviews)/src/components/ReviewsBlock/ReviewsBlock.tsx
@@ -6,27 +6,24 @@ type ReviewsBlockProps = {
     reviews: ReviewData[];
 };
 
-export default function ReviewsBlock({ reviews }: ReviewsBlockProps): JSX.Element {
-    if (reviews.length)
-    {
-        return (
-            <div className="reviews-block">
-                <div className="reviews">
-                    {reviews.map((review, index) => (
-                    <Review
-                        key={index}
-                        name={review.name}
-                        photo={review.photo}
-                        rating={review.rating}
-                        text={review.text}
-                    />
-                    ))}
-                </div>
+export default function ReviewsBlock({ reviews }: ReviewsBlockProps): React.ReactNode {
+    if (!reviews.length) {
+        return null;
+    }
+
+    return (
+        <div className="reviews-block">
+            <div className="reviews">
+                {reviews.map((review, index) => (
+                <Review
+                    key={index}
+                    name={review.name}
+                    photo={review.photo}
+                    rating={review.rating}
+                    text={review.text}
+                />
+                ))}
             </div>
-        )
-    }
-    else
-    {
-        return (<></>)
-    }
+        </div>
+    )
 }
